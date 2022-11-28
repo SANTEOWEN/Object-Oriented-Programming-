24591b5ca6f43c8714a093858588554d11656cba
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2022-11-28 13:49:50 +0800

    I created Objects and attributes under the class

---
+++ oop.py
@@ -0,0 +1,19 @@
+#Creating Objects#
+class Agi_Character:
+    name = "Name"
+    HealtPoints = "700"
+    ManaPoints = "267"
+    Attack_Damage = "60"
+
+#Creating objects under the class name Agi_Character"
+Agi_one = Agi_Character()
+Agi_two = Agi_Character()
+
+
+#accessing attributes/ puting some details on an object#
+Agi_one.name = "URSA"
+
+Agi_two.name = "PhantomAssasin"
+
+class Products:
+    
