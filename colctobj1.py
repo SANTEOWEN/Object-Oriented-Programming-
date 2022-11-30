fc6eea513854c23495df41f3be12a2e579b49eab
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2022-11-30 11:47:25 +0800

    Using loop for reading data!
    We uses for loop to read all the gathered or input datas from the user

---
+++ colctobj1.py
@@ -0,0 +1,15 @@
+class person:
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print("lamaw " + self.name)
+
+#We can also use a loop to read a collection
+pOne = person("Owen")
+pTwo = person("Dave")
+pThree = person("Joshua")
+
+listofpeps = [pOne, pTwo, pThree]
+for person in listofpeps:
+    print(person.name)
